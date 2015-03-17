0b6db96daf7f4d51426d6d815d5664fddd4fc599
alkresin <alkresin@yahoo.com>
2015-03-17 09:58:42 +0400

    sh files added

---
+++ clear.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+. ./setenv.sh
+
+rm -f src/$PACKAGE_PATH/R.java
+rm -f assets/*
+rm -f bin/*
+rm -f *.out
+rm -f -r lib
+mkdir lib
+chmod a+w+r+x lib
+rm -f -r libs
+mkdir libs
+chmod a+w+r+x libs
+rm -f -r obj
+mkdir obj
+chmod a+w+r+x obj
---
+++ setenv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export HDROIDGUI=/apps/android/projects/hDroidGUI
+. $HDROIDGUI/setenv.sh
+
+export APPNAME=hddbf
+export PACKAGE=su.harbour.hddbf
+export PACKAGE_PATH=su/harbour/hddbf
+export MAIN_CLASS=MainActivity
+export DEV_HOME=`pwd`
