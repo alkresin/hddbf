6e4096e3192dcff211caf6df63e6429f32448822
alkresin <alkresin@yahoo.com>
2015-03-31 20:49:28 +0300

    java files updates due to hDroidGUI changes

--- src/su/harbour/hddbf/MainActivity.java
+++ src/su/harbour/hddbf/MainActivity.java
@@ -1,57 +1,16 @@
 package su.harbour.hddbf;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import su.harbour.hDroidGUI.*;
 
-public class MainActivity extends Activity {
-
-   private static View mainView;
+public class MainActivity extends HDActivity {
 
    @Override
    public void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
 
+      bMain = true;
       MainApp.harb.setDopClass( DopActivity.class );
-      mainView = MainApp.harb.createAct( this, null );
-      setContentView( mainView );
-      MainApp.harb.hrbCall( "HD_INITWINDOW","0" );
+      super.onCreate(savedInstanceState);
    }
-
-   @Override
-   protected void onDestroy() {
-      super.onDestroy();
-
-      MainApp.harb.closeAct( "0" );
-   }
-   @Override
-   protected void onResume() {
-       super.onResume();
-
-      MainApp.harb.setContext( this,mainView );
-   }
-
-   //@Override
-   //public void onWindowFocusChanged (boolean hasFocus) {
-    //  super.onWindowFocusChanged (hasFocus);
-    //  if( hasFocus )
-    //     MainApp.harb.doActions();
-   //}
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-       MainApp.harb.SetMenu( menu );
-       return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-       MainApp.harb.onMenuSel( item.getItemId() );
-       return true;
-    }
-
 
 }
